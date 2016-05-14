2894f7d0e51f9e79ed04058747e9c563769e8b42
ryosaito999 <ryocsaito@gmail.com>
2016-05-14 16:18:55 -0700

    Random cup spawn works

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class gameManager : MonoBehaviour {
+
+    public static List<Vector3> spawnLocations;
+    public GameObject cup;
+    private void spawnCup()
+    {
+        int x = Random.Range(0, 2);
+        Instantiate(cup, spawnLocations[x], Quaternion.identity);             
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        spawnLocations = new List<Vector3>();
+        spawnLocations.Add(new Vector3(-.38f, 2.39f, -2.22f));
+        spawnLocations.Add(new Vector3(-7.25f, 2.28f, -2.22f));
+        spawnLocations.Add(new Vector3(-.38f, 2.39f, 3.22f));
+
+    }
+    void Start () {
+        spawnCup();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
